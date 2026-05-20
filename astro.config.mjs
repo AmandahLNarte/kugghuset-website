d0b0085308823df236443b84b46206a51c945d0b
Evgenia Nordholm <evgenia@kugghuset.se>
2026-05-20 14:02:22 +0200

    Fix #1: Use production canonical URL (kugghuset.se)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://www.kugghuset.se',
+  site: 'https://kugghuset.se',
   integrations: [tailwind()],
 });
